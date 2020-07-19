f9c00817114d2caf2bf76770b63d84c71753cb0b
JosymarM <josymar.mayhua@gmail.com>
2020-07-19 18:24:55 -0500

    Dependencias

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const handleBars = require('express-handlebars').create();
+const publicDir = require('path').join(__dirname, 'public');
 const router = express.Router();
 const app = express();
 
+//
+// Importing routes
+const indexRoutes = require('./routes/index');
+
 //SETTINGS
 app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'views'));
 
 //MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -18,9 +26,11 @@ app.use(cookieParser('secret'));
 app.use(session({cookie: {maxAge: null}}));
 app.engine('handlebars', handleBars.engine);
 app.set('view engine', 'handlebars');
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(publicDir));
+//routes
+app.use('/', indexRoutes);
 
 
 
