9663b689221c5f05aacb15d6a1a8ce859a98060e
JosymarM <josymar.mayhua@gmail.com>
2020-07-19 23:15:07 -0500

    Dependencias

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const handleBars = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cookieParser('secret'));
-app.use(session({cookie: {maxAge: null}}));
-
 // View engine setup
-app.engine('handlebars', handleBars());
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //MIDDLEWARES
 app.use(morgan('dev'));
@@ -25,8 +22,8 @@ app.set('port', process.env.PORT || 8088);
 
 //HANDLE PAGES
 app.get('/', (req, res) => {
-    res.render('home');
-})
+    res.render('contact');
+});
 
 
 // Starting the server
